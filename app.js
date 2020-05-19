87e6b0090710020da20c008b02768997ab987c37
Palaash Atri <palaash.atri@gmail.com>
2020-05-19 23:45:41 +0530

    ES6 Modification, add API environment variable

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-var express = require('express');
-var app = express();
-var request = require('request');
+const express = require('express'),
+    app = express(),
+    request = require('request'),
+    dotenv = require("dotenv");
+
+dotenv.config();
+const apikey =  process.env.API_KEY; //from the web developer bootcamp - colt steele (udemy)
 
 app.set("view engine","ejs");
 
@@ -10,7 +14,7 @@ app.get("/",function(req,res){
 
 app.get("/results",function(req,res){
     var query = req.query.search;
-    var url = "http://omdbapi.com/?s="+query+"&apikey=thewdb";
+    var url = `http://omdbapi.com/?s=${query}&apikey=${apikey}`;
     request(url,function(error,response,body){
       if(!error && response.statusCode == 200){
           var data = JSON.parse(body);
