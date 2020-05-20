83358eab6ea010132819127663cdc94b57c6db06
Palaash Atri <palaash.atri@gmail.com>
2020-05-20 22:30:57 +0530

    added functional comments & boostrap 4 cdn

--- app.js
+++ app.js
@@ -14,17 +14,17 @@ app.get("/",function(req,res){
 });
 
 app.get("/results",function(req,res){
-    var query = req.query.search;
-    var url = `http://omdbapi.com/?s=${query}&apikey=${apikey}`;
-    request(url,function(error,response,body){
-      if(!error && response.statusCode == 200){
-          var data = JSON.parse(body);
-          res.render("results",{data:data,query:query});
+    let query = req.query.search; // get search query from "home.ejs"
+    let url = `http://omdbapi.com/?s=${query}&apikey=${apikey}`; // create a template url to pass to the request function below
+    request(url,function(error,response,body){ // callback function with parameters error, response and body from request
+      if(!error && response.statusCode == 200){ // no error and successful response
+          var data = JSON.parse(body); //body is in the form of string, so parse it into JSON
+          res.render("results",{data:data,query:query}); // render "results.ejs" with parameters data & query defined in the ejs template
       }  
     })
 });
 
 
-app.listen(process.env.PORT || 5000,function () {
+app.listen(process.env.PORT || 5005,function () {
     console.log('Movie app is running!');
 });
