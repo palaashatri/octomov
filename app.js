29e0f5d35b3b99cd08df575966931b94064b5fcc
Palaash Atri <palaash.atri@gmail.com>
2020-05-23 00:27:13 +0530

    fixed a couple little bugs 😋

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ app.get("/",(req,res)=>{
 app.get("/results",(req,res)=>{
     let query = req.query.search; // get search query from "home.ejs"
     let url = `http://omdbapi.com/?s=${query}&apikey=${apikey}`; // create a template url to pass to the request function below
-    request(url,function(error,response,body){ // callback function with parameters error, response and body from request
+    request(url,(error,response,body)=>{ // callback function with parameters error, response and body from request
       if(!error && response.statusCode == 200){ // no error and successful response
-          var data = JSON.parse(body); //body is in the form of string, so parse it into JSON
+          let data = JSON.parse(body); //body is in the form of string, so parse it into JSON
           res.render("results",{data:data,query:query}); // render "results.ejs" with parameters data & query defined in the ejs template
       }  
     })
